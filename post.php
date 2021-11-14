b8cca7d008024024371b4d09d9610668d8cf7d1b
Yusuf Rasit Ozer <72783563+yusufrasitozer@users.noreply.github.com>
2021-11-14 14:26:27 +0300

    Read more button deleted.
    Read more button in the post page has deleted successfully.

--- post.php
+++ post.php
@@ -54,7 +54,6 @@ include "includes/header.php";
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 <?php } ?>
