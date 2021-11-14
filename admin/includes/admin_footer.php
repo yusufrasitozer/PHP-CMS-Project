6b5ac2d3cf309634878938ac4f66d00057e4c050
Yusuf Rasit Ozer <72783563+yusufrasitozer@users.noreply.github.com>
2021-11-14 16:09:38 +0300

    CK Editor script added
    CK Editor script  has added successfully.

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -6,6 +6,13 @@
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
+
+    <!-- ckeditor -->
+
+    <script src="https://cdn.ckeditor.com/ckeditor5/31.0.0/classic/ckeditor.js"></script>
+
+    <script src="js/scripts.js"></script>
+
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
