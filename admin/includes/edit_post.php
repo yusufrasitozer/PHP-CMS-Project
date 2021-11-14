e9a2ed4ddba5a377f62aa6e35cbbf11b8edee3f7
Yusuf Rasit Ozer <72783563+yusufrasitozer@users.noreply.github.com>
2021-11-14 15:32:06 +0300

    Post status selection change

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -135,12 +135,33 @@ $select_posts_by_id = mysqli_query($connection, $query);
       
       </div>
 
+    <div class="form-group">
       
-      <div class="form-group">
-         <label for="post_status">Post Status</label>
-         <input value="<?php echo $post_status; ?>"   type="text" class="form-control" name="post_status" >
-      </div>
+      <select name="post_status" id="">
+
+       <option value='<?php echo $post_status ?>'><?php echo $post_status; ?></option>
+       
+       <?php 
+       if($post_status == 'published' ) {
+
+         echo "<option value='draft'> Draft</option>";
+
+
+       } else {
+
+         echo "<option value='published'> Publish</option>";
+
+       }
+
+       
+
+       ?>
+      </select>
+
+    </div>
+
       
+   
       
       
     <div class="form-group">
