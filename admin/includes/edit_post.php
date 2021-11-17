3a9f799a3fe9411990137380331fa833b0007dc4
Yusuf Rasit Ozer <72783563+yusufrasitozer@users.noreply.github.com>
2021-11-17 18:23:08 +0300

    View post page fixed.

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -67,7 +67,7 @@ $select_posts_by_id = mysqli_query($connection, $query);
 
                   confirmQuery($update_post);
 
-                  echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'>View Post</a> or <a href='../posts.php'>Edit More Posts</a></p>";
+                  echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
 
                   
 
