58e9cdcbdc5f956dd67adbab1f8fa32894c721a2
Yusuf Rasit Ozer <72783563+yusufrasitozer@users.noreply.github.com>
2021-11-18 20:42:42 +0300

    Salt security added to the passwords.

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 13 Kas 2021, 15:22:18
+-- Üretim Zamanı: 18 Kas 2021, 18:41:46
 -- Sunucu sürümü: 10.4.21-MariaDB
 -- PHP Sürümü: 7.4.24
 
@@ -64,7 +64,9 @@ CREATE TABLE `comments` (
 INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
 (1, 10, 'Joseph', 'joseph@joseph.com', 'This is just an example', 'approved', '2021-11-09'),
 (4, 9, 'author', 'y@y.com', 'asdasdas', 'unapproved', '2021-11-10'),
-(5, 9, 'Jan valjean', 'j@j.com', 'Hey t', 'unapproved', '2021-11-10');
+(5, 9, 'Jan valjean', 'j@j.com', 'Hey t', 'unapproved', '2021-11-10'),
+(7, 14, 'asd', 'asd@a.com', 'asdasd', 'unapproved', '2021-11-14'),
+(8, 14, 'hey', 'hey@ye.com', 'yes', 'unapproved', '2021-11-18');
 
 -- --------------------------------------------------------
 
@@ -90,8 +92,13 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES
-(14, 1, 'New post', 'Joseph', '2021-11-13', '2.png', 'Hello This is post content                  ', 'post, first, here', 0, 'published'),
-(15, 1, 'Another Post', 'Joseph', '2021-11-13', 'LaravelLogo.png', '         Another post content', 'laravel', 0, 'published');
+(14, 1, 'New post', 'Joseph', '2021-11-17', '2.png', '<p>Hello This is post content</p>', 'post, first, here', 2, 'published'),
+(15, 1, 'Another Post', 'Joseph', '2021-11-15', 'LaravelLogo.png', '<p>Another post content</p>', 'laravel', 0, 'published'),
+(18, 2, 'Example Post', 'joseph', '2021-11-17', '', '<p>here</p>', 'tag', 0, 'published'),
+(19, 1, 'asd', 'asd', '2021-11-17', '', '<p>asd</p>', 'asd', 0, 'published'),
+(20, 1, 'yes', 'here', '2021-11-17', '', '<p>dsa</p>', 'dsa', 0, 'add'),
+(21, 1, 'ses', 'ses', '2021-11-17', '', '<p>asd</p>', 'asd', 0, 'published'),
+(22, 1, 'Another post', 'Joseph', '2021-11-18', '', '<p>asd</p>', 'asd', 0, 'published');
 
 -- --------------------------------------------------------
 
@@ -108,7 +115,7 @@ CREATE TABLE `users` (
   `user_email` varchar(255) NOT NULL,
   `user_image` text NOT NULL,
   `user_role` varchar(255) NOT NULL,
-  `randSalt` varchar(255) NOT NULL
+  `randSalt` varchar(255) NOT NULL DEFAULT '$2y$10$iusesomecrazystring22'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -119,8 +126,12 @@ INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `
 (1, 'josepha', '123', 'joseph', 'ozerrra', 'joseph@j.com', '', 'subscriber', ''),
 (4, 'dd', 'dsa', 'a', 'd', 'user1@gmail.com', '', 'admin', ''),
 (5, 'asd', 'asd', 'asda', 'asdasa', 'asdasd@dsa.com', '', 'subscriber', ''),
-(6, 'a', 'a', 'abc', 'a', 'a@a.com', '', 'subscriber', ''),
-(7, 'b', 'b', 'b', 'b', 'b@b.com', '', 'admin', '');
+(6, 'a', 'a', 'abc', 'a', 'a@a.com', '', 'admin', ''),
+(7, 'b', 'b', 'b', 'b', 'b@b.com', '', 'admin', ''),
+(10, 'ccz', 'c', 'ccz', 'ccz', 'ccz@c.com', '', 'admin', ''),
+(11, 'dsasd', 'qweqwe', 'adsasd', 'sdasd', 'qwe@dc.c', '', 'admin', '$2y$10$iusesomecrazystring22'),
+(12, 'demo', 'qwe', '', '', 'demo@d.com', '', 'subscriber', '$2y$10$iusesomecrazystring22'),
+(13, 'asd', 'asd', '', '', 'asd@d.com', '', 'subscriber', '$2y$10$iusesomecrazystring22');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -164,19 +175,19 @@ ALTER TABLE `categories`
 -- Tablo için AUTO_INCREMENT değeri `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
