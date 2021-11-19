fd9c4226b416625ccc3a9da15025ca74dc68c81e
Yusuf Rasit Ozer <72783563+yusufrasitozer@users.noreply.github.com>
2021-11-19 14:53:12 +0300

    password encrypted match

--- includes/login.php
+++ includes/login.php
@@ -37,6 +37,8 @@ if(!$select_user_query) {
 
 }
 
+$password = crypt($password, $db_user_password);
+
 if($username !== $db_username && $password !== $db_user_password) {
 
     header("Location: ../index.php");
@@ -48,15 +50,12 @@ if($username !== $db_username && $password !== $db_user_password) {
     $_SESSION['lastname'] = $db_user_lastname;
     $_SESSION['user_role'] = $db_user_role;
 
+   
     
 
 
     header("Location: ../admin");
-} else {
-    
-    header("Location: ../index.php");
-}
-
+} 
 
 
 
